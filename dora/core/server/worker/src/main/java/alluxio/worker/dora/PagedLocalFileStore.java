2fe4c407c9ef2cd3931d7d435b57f66ef2417a5e
Bowen Ding <6999708+dbw9580@users.noreply.github.com>
2023-05-16 18:30:40 +0800

    idea

---
+++ dora/core/server/worker/src/main/java/alluxio/worker/dora/PagedLocalFileStore.java
@@ -0,0 +1,43 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.worker.dora;
+
+import alluxio.file.FileId;
+
+import java.util.List;
+
+public class PagedLocalFileStore implements LocalFileStore, MutableFileStoreMixin {
+  @Override
+  public LocatedFile locateFile(FileId fileId) {
+    return null;
+  }
+
+  @Override
+  public LocatedFile locateFile(File file) {
+    return null;
+  }
+
+  @Override
+  public List<ScopedFileStore> listSubstores() {
+    return null;
+  }
+
+  @Override
+  public void deleteFile(LocatedFile file) {
+
+  }
+
+  @Override
+  public MutableFile createFile(FileId fileId) {
+    return null;
+  }
+}
